5355806c141c16f14a44ac83db6a24673989c4b1
eviluser <jendrekpb@gmail.com>
2022-01-21 18:07:22 +0100

    Added 4th call example

--- patch_with_iterable/test_something.py
+++ patch_with_iterable/test_something.py
@@ -7,6 +7,9 @@ def test_iterable_patching(mocker):
     first_call = random.randint(10, 100)
     second_call = random.randint(10, 100)
     third_call = random.randint(10, 100)
+    
+    # Will raise an error
+    # fourth_call = random.randint(1, 5)
 
     assert first_call == -5
     assert second_call == 300
